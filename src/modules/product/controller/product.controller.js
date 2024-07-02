35552a40658f6c1700337bbbcf6d40622c08fb30
Dion Arya Pamungkas <dionarya.p@gmail.com>
2024-07-02 15:13:26 +0700

    change response

--- src/modules/product/controller/product.controller.js
+++ src/modules/product/controller/product.controller.js
@@ -4,21 +4,24 @@ module.exports = {
   getAllProducts: async (request, h) => {
     const { page = 1, limit = 10, q } = request.query;
     const offset = (page - 1) * limit;
-    const response = await productService.getAllProducts({
+    const responseService = await productService.getAllProducts({
       page,
       limit,
       offset,
       search: q
     });
-    return h.response(response).code(response.status);
+    const { status, ...response } = responseService
+    return h.response(response).code(status);
   },
   getDetailProduct: async (request, h) => {
     const { product_id } = request.params;
-    const response = await productService.getDetailProduct({product_id})
-    return response;
+    const responseService = await productService.getDetailProduct({product_id});
+    const { status, ...response } = responseService;
+
+    return h.response(response).code(status);
   },
   deleteProduct: async (request, h) => {
-
+  
   },
   importProduct: async (request, h) => {
 
